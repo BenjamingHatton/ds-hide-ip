2ad99d148a857e70397db8e3e43467c524e1b8f0
BenjamingHatton <155596362+BenjamingHatton@users.noreply.github.com>
2024-10-16 14:42:49 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -18,7 +18,12 @@ after_initialize do
       if [1, 2, 3].include?(self.id)
         'hidden IP address'
       else
-        super
+        begin
+          super
+        rescue => e
+          Rails.logger.error("Error in ip_address: #{e.message}")
+          nil
+        end
       end
     end
   end
